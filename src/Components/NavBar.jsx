f3a451dd445bc2a1acfe4a1dc5221800aacb5112
daniel amekpoagbe <amekpoagbedaniel@gmail.com>
2025-04-30 17:53:24 +0000

    fixed the navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import { Menu, X, Home, Scissors } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { motion } from "framer-motion";
 import { FaWhatsapp, FaInstagram, FaTiktok } from "react-icons/fa";
 
